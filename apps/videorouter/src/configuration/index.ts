a4611bf6ce179d045b61cb289d9a99b78cab20b2
Oussama <ojaaouani@x2omedia.com>
2025-03-20 14:58:54 -0400

    update to import url

--- apps/videorouter/src/configuration/index.ts
+++ apps/videorouter/src/configuration/index.ts
@@ -1,11 +1,14 @@
 import { createLogger } from "@ojaaouani/logger/index";
 import { loadConfiguration } from "@ojaaouani/configuration/index";
-import { join, normalize } from "node:path";
+import { dirname, normalize, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+    
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export const configurationLogger = createLogger({
   serviceName: "@configuration",
 });
 
 export const configuration = loadConfiguration(
-  normalize(join(import.meta.dirname, "../../../../router.config.toml"))
+  normalize(join(__dirname, "../../../../router.config.toml"))
 );
